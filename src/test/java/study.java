c24f0e08a5a4a6ad7e0e3aed8149a7825de1086b
yusuke <yusuke.m0902@gmail.com>
2016-12-31 19:45:03 +0900

    first_commit

---
+++ src/test/java/study.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+//test selnium
+public class study {
+
+	public static void main(String[] args){
+
+		//WebDriver driver = new FirefoxDriver();
+		File file = new File("C:/pleiades/workspace/test_selenium_gradle/driver/IEDriverServer.exe");
+
+		System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+		InternetExplorerDriver driver = new InternetExplorerDriver();
+
+
+		driver.get("http://example.selenium.jp/reserveApp/");
+        driver.findElement(By.id("guestname")).sendKeys("サンプルユーザ");
+        driver.findElement(By.id("goto_next")).click();
+        driver.quit();  	
+	}
+
+}
+
+
